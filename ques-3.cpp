a50d3ad1739b8c7f9ff6f106b13f2559bfe04177
ajmanipriyancy <ajmanipriyancy@gmail.com>
2025-09-15 15:24:14 +0530

    ques-3.cpp

---
+++ ques-3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int i;
+    int arr[5] = {1};   // initialize first element as 1, rest default to 0
+    for (i = 0; i < 5; i++)
+        printf("%d", arr[i]);
+    return 0;
+}
+
+
+arr = {1, 0, 0, 0, 0}
+
+output:
+10000
